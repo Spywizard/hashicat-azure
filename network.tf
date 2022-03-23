0b2222d86c9224d7e5aec6018a2819e37cc3173f
Spywizard <peter.chesarek@gmail.com>
2022-03-23 16:35:36 +0000

    added network module

--- network.tf
+++ network.tf
@@ -3,6 +3,6 @@ module "network" {
   source  = "Azure/network/azurerm"
   version = "3.5.0"
   # insert the 1 required variable here
-  recourse_group_name = azurerm_resource_group.myresourcegroup.name
+  resourse_group_name = azurerm_resource_group.myresourcegroup.name
 }
 
